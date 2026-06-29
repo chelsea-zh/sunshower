fa45cb561c6ac10db4eab0bc1def54b902346e16
chelsea-zh <chelseazh2025@gmail.com>
2026-06-29 11:55:38 -0700

    can pick flowers

--- scripts/flower.gd
+++ scripts/flower.gd
@@ -9,6 +9,7 @@ func _ready() -> void:
 	$AnimatedSprite2D.frame = 0
 	$AnimatedSprite2D.animation = type[randi_range(0, 3)]
 	$Control.mouse_filter = Control.MOUSE_FILTER_PASS
+	picked = false
 
 func _process(_delta: float) -> void:
 	if clockwise:
@@ -20,11 +21,14 @@ func _process(_delta: float) -> void:
 		if rotation_degrees <= -15:
 			clockwise = true
 		
+	if picked:
+		global_position.x = get_global_mouse_position().x
+		global_position.y = get_global_mouse_position().y + 75
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is raindrop:
-		if randf() > 0.5 and $AnimatedSprite2D.frame < 4:
+		if randf() > 0 and $AnimatedSprite2D.frame < 4:
 			$AnimatedSprite2D.frame += 1
 	pass # Replace with function body.
 
@@ -32,12 +36,19 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 func _on_area_2d_mouse_entered() -> void:
 	if $AnimatedSprite2D.frame == 4:
 		$AnimatedSprite2D.frame = 5
-	$Control.mouse_filter = Control.MOUSE_FILTER_STOP
+		#$Control.mouse_filter = Control.MOUSE_FILTER_STOP
 	pass # Replace with function body.
 
 
 func _on_area_2d_mouse_exited() -> void:
 	if $AnimatedSprite2D.frame == 5:
 		$AnimatedSprite2D.frame = 4
-	$Control.mouse_filter = Control.MOUSE_FILTER_PASS
+		#$Control.mouse_filter = Control.MOUSE_FILTER_PASS
+	pass # Replace with function body.
+
+
+func _on_area_2d_picked() -> void:
+	if $AnimatedSprite2D.frame >= 4:
+		picked = not picked
+		print(picked)
 	pass # Replace with function body.
---
+++ scripts/flower_area_2d.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+signal picked
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			picked.emit()
+			viewport.set_input_as_handled() 
