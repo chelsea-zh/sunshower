6e54777572d32def429d4a3e67e96046e1483278
chelsea-zh <chelseazh2025@gmail.com>
2026-06-29 10:57:35 -0700

    flowers grow

--- scripts/flower.gd
+++ scripts/flower.gd
@@ -1,10 +1,14 @@
 class_name flower extends CharacterBody2D
 
 var clockwise = true
+var type = ["daisy", "hydrangea", "orchid", "tulip"]
+var picked = false
 
 func _ready() -> void:
 	rotation_degrees = 0
 	$AnimatedSprite2D.frame = 0
+	$AnimatedSprite2D.animation = type[randi_range(0, 3)]
+	$Control.mouse_filter = Control.MOUSE_FILTER_PASS
 
 func _process(_delta: float) -> void:
 	if clockwise:
@@ -20,5 +24,20 @@ func _process(_delta: float) -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is raindrop:
-		pass
+		if randf() > 0.5 and $AnimatedSprite2D.frame < 4:
+			$AnimatedSprite2D.frame += 1
+	pass # Replace with function body.
+
+
+func _on_area_2d_mouse_entered() -> void:
+	if $AnimatedSprite2D.frame == 4:
+		$AnimatedSprite2D.frame = 5
+	$Control.mouse_filter = Control.MOUSE_FILTER_STOP
+	pass # Replace with function body.
+
+
+func _on_area_2d_mouse_exited() -> void:
+	if $AnimatedSprite2D.frame == 5:
+		$AnimatedSprite2D.frame = 4
+	$Control.mouse_filter = Control.MOUSE_FILTER_PASS
 	pass # Replace with function body.
